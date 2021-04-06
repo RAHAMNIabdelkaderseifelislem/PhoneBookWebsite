32bd026bdbb7990456f2779d200c33248f6c6f33
RAHMANI Abd El Kader Seif El Islem <77025510+RAHAMNIabdelkaderseifelislem@users.noreply.github.com>
2021-04-06 17:08:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include 'functions.php';
+?>
+<?=template_header('HOME')?>
+<div>
+    <h2>Home</h2>
+    <p>Welcome To Home Page!</p>
+    <p>I don't have great skills for design sorry</p>
+</div>
+<?=template_footer()?>
